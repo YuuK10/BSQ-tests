d330f83ffc17d5567d1eb78e2c992b04041ff890
Loic Siquet <lsiquet@e-r8-p3.s19.be>
2018-07-25 20:59:44 +0200

    Added 19 logo header

--- test_my_bsq_please.sh
+++ test_my_bsq_please.sh
@@ -1,17 +1,21 @@
-echo "Ok, let's test your BSQ.\n"
+cat header.md
+echo "\nOk, let's test your BSQ."
+echo "========================\n"
 echo "I am creating a 'results' directory where you'll find the results"
 mkdir results > /dev/null 2>&1
 echo "Now testing small to big tables..."
 sh test_table.sh > results/test_table.txt
 echo "Now testing multi arguments..."
 sh test_multi_arg.sh > results/test_multi_arg.txt
-echo "Now testing bad tables (just to see how it reacts)..."
+echo "Now testing bad tables...\n(If it doesn't react, think about checking directory reading error)"
 sh bad_tests.sh > results/test_bad.txt
-echo "\nLet's compare your results hashs with the reference one...\n"
+echo "\n========================\n"
+echo "Let's compare your results hashs with the reference one...\n"
 
 md5 results/test_table.txt results/test_multi_arg.txt results/test_bad.txt | cut -d ' ' -f 4 > results/hash.txt
 
 DIFF="$(diff -u results/hash.txt hash_ref.txt)"
+
 if [ -z "$DIFF" ]
 then
     echo "It looks the same, your BSQ passes all the tests !"
