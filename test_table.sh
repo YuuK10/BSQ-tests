efbe63d0d8307d98d952b10832f41b3c1798472b
Loic Siquet <lsiquet@e-r8-p3.s19.be>
2018-07-25 22:02:36 +0200

    Added vertical and horizontal tests

--- test_table.sh
+++ test_table.sh
@@ -1,10 +1,14 @@
 echo "================ Tables tests ================\n"
 
-echo "===== Test 5 ================\n"
+echo "===== Test vertical==========\n"
+../bsq tables/test_vert.txt
+echo "\n===== Test horizontal =======\n"
+../bsq tables/test_hor.txt
+echo "\n===== Test 5 ================\n"
 ../bsq tables/test_5.txt
-echo "===== Test 39-80 ================\n"
+echo "\n===== Test 39-80 ============\n"
 ../bsq tables/test_39-80.txt
-echo "===== Test 5 ================\n"
+echo "\n===== Test 68-27 ============\n"
 ../bsq tables/test_68-27.txt
 echo "\n===== Test 100 ==============\n"
 ../bsq tables/test_100.txt
