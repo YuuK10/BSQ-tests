2b5c1959dff7ff6eef849e92241173136e2b9ee1
Loic Siquet <lsiquet@e-r8-p3.s19.be>
2018-07-25 20:27:01 +0200

    Added to more tests concerning line count of the map

--- bad_tests.sh
+++ bad_tests.sh
@@ -5,4 +5,6 @@ echo "================ Error tests ================\n"
 ../bsq tables/bad/test_100_bad_4.txt 2>&1
 ../bsq tables/bad/test_100_bad_5.txt 2>&1
 ../bsq tables/bad/test_100_bad_6.txt 2>&1
+../bsq tables/bad/test_100_bad_7.txt 2>&1
+../bsq tables/bad/test_100_bad_8.txt 2>&1
 ../bsq bad_file 2>&1
