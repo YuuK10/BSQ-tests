f5660597a25f13aaaea205d269b7944884a7942f
Loic Siquet <lsiquet@e-r8-p3.s19.be>
2018-07-25 20:43:04 +0200

    Added tests with echo for stdin read

--- test_multi_arg.sh
+++ test_multi_arg.sh
@@ -1,4 +1,7 @@
 echo "============ Multi arguments test ============\n"
 ../bsq tables/test_39-80.txt tables/test_68-27.txt tables/test_5.txt tables/test_100.txt
+
+echo "1.ox\n....." | ../bsq 2>&1
+echo "Hello world !" | ../bsq 2>&1
 cat tables/test_5.txt | ../bsq
 cat tables/test_100.txt | ../bsq
