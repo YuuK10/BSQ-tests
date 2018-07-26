cf13df6e389fae279c6a21ec313044a3aeb54764
Loic Siquet <lsiquet@e-r8-p3.s19.be>
2018-07-26 10:21:06 +0200

    Added standard tests

--- test_table.sh
+++ test_table.sh
@@ -1,4 +1,29 @@
 echo "================ Tables tests ================\n"
+echo "=============== Standard tests ==============="
+echo "\n===== Test 1 ================\n"
+../bsq tables/test_1.txt
+echo "\n===== Test 1 full ===========\n"
+../bsq tables/test_1_full.txt
+echo "\n===== Test 5 ================\n"
+../bsq tables/test_5.txt
+echo "\n===== Test 8 ================\n"
+../bsq tables/test_8.txt
+echo "\n===== Test 10 ===============\n"
+../bsq tables/test_10.txt
+echo "\n===== Test charset ==========\n"
+../bsq tables/test_charset.txt
+echo "\n===== Test 100 ==============\n"
+../bsq tables/test_100.txt
+echo "\n===== Test 100 full =========\n"
+../bsq tables/test_100_full.txt
+echo "\n===== Test 100 empty ========\n"
+../bsq tables/test_100_empty.txt
+echo "\n===== Test 100 big sol ======\n"
+../bsq tables/test_100_big_sol.txt
+echo "\n===== Test 1000 =============\n"
+../bsq tables/test_1000.txt
+
+echo "================ Custom tests ==============="
 
 echo "===== Test vertical =========\n"
 ../bsq tables/test_vert.txt
@@ -8,18 +33,12 @@ echo "\n===== Test vertical 2========\n"
 ../bsq tables/test_vert2.txt
 echo "\n===== Test horizontal 2 =====\n"
 ../bsq tables/test_hor2.txt
-echo "\n===== Test 5 ================\n"
-../bsq tables/test_5.txt
 echo "\n===== Test 39-80 ============\n"
 ../bsq tables/test_39-80.txt
 echo "\n===== Test 68-27 ============\n"
 ../bsq tables/test_68-27.txt
-echo "\n===== Test 100 ==============\n"
-../bsq tables/test_100.txt
 echo "\n===== Test 500 ==============\n"
 ../bsq tables/test_500.txt
-echo "\n===== Test 1000 =============\n"
-../bsq tables/test_1000.txt
 echo "\n===== Test 5000 =============\n"
 ../bsq tables/test_5000.txt
 echo "\n===== Test 10000 ============\n"
