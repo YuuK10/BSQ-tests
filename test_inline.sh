782352315d422976d3266f5b433fd09276563043
root <root@localhost.localdomain>
2018-08-16 14:36:52 -0400

    Removed all files from branch, except ref_results

--- test_inline.sh
+++
@@ -1,6 +0,0 @@
-echo "Executing standard input tests...\n"
-
-echo "1.ox\n....." | ../bsq 2>&1 > results/stdin_01.txt
-echo "Hello world !" | ../bsq 2>&1 > results/stdin_02.txt
-cat tables/test_5.txt | ../bsq 2>&1 > results/stdin_03.txt
-cat tables/test_100.txt | ../bsq 2>&1 > results/stding_04.txt
--- test_multi_arg.sh
+++
@@ -1,3 +0,0 @@
-echo "Executing multi arguments tests...\n"
-(../bsq tables/test_39-80.txt tables/test_68-27.txt tables/test_5.txt tables/test_100.txt 2>&1) > results/multi_arg_01.txt
-(../bsq tables/test_5.txt tables/test_10.txt "Hello World !" tables/test_8.txt 2>&1) > results/multi_arg_02.txt
